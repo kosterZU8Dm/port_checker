93131041221e7196b5e16f34f886e42d30c58ad6
kosta_wsl <kosta_wsl@kostamail.com>
2024-01-23 00:48:59 +0300

    a useful utility was created

--- README.md
+++ README.md
@@ -2,5 +2,20 @@
 ---
 106 - session is not close\
 111 - connection refused\
-9 -\
+9 - ...\
 ---
+# About Socket library
+AF_INET = ipv4 addresses\
+SOCK_STREAM = means tcp socket
+---
+### First problem. 23.01.2024
+1. I catch error, when I made socket outside loop of for. For fix I need move creation of socket to loop inside function.
+2. I need to use function socket.connect_ex instead of socket.connect, because get an error code is better than get exception call
+3. I need to increase a timeout of connect to port
+
+### Usage
+```
+source venv/bin/active
+pip install -r requirements.txt
+python3 app.py 127.0.0.1
+```
--- app.py
+++ app.py
@@ -7,10 +7,6 @@ ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
 ip_address = sys.argv[1]
 ports_list = [80, 443, 25, 22, 8080, 2222, 21, 23, 5432, 3306]
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#AF_INET = ipv4 addresses
-#SOCK_STREAM = means tcp socket
-
 print(ascii_banner)
 print(border)
 print(f'Scanning Target: {ip_address}')
@@ -18,20 +14,19 @@ print(border)
 
 def check_port(ip_a):
     try:
-
         for x in ports_list:
-            socket.setdefaulttimeout(1)
-            connection_result = sock.connect((ip_a, x))
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(2)
+            connection_result = sock.connect_ex((ip_a, x))
             #connection_result = sock.connect_ex((ip_a, x))
             #Возвращает номер ошибки, вместо вызова ошибки при выполнении 
             if connection_result == 0:
-                print(f'port {x} is open')
+                print(f'port {x} is open. Success')
             elif connection_result == 111:
-                print(f'port {x}, connection refused')
+                print(f'port {x}, connection refused. Fail')
             else:
                 print(f'port {x} is close. Code is {connection_result}')
             sock.close()
-
     except KeyboardInterrupt:
         print("\n Exiting Program")
         sys.exit()
